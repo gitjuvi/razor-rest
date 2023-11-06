f56f984a4098d34261d2ca7d5af830f8f565254a
gitjuvi <57840737+gitjuvi@users.noreply.github.com>
2023-11-07 01:55:39 +0200

    Add files via upload

---
+++ Pages/LueVitsit.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor.Pages;
+
+public class LueVitsitModel : PageModel
+{
+    public string Otsikko { get; set; }
+    public string Vitsiteksti { get; set; }
+    
+    private readonly ILogger<LueVitsitModel> _logger;
+
+    public LueVitsitModel(ILogger<LueVitsitModel> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ Pages/TeeVitsi.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor.Pages;
+
+public class TeeVitsiModel : PageModel
+{
+    public string Otsikko { get; set; }
+    public string Vitsiteksti { get; set; }
+    private readonly ILogger<TeeVitsiModel> _logger;
+
+    public TeeVitsiModel(ILogger<TeeVitsiModel> logger)
+    {
+        _logger = logger;
+    }
+}
